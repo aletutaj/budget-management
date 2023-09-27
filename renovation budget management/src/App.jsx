b1063d1e4d86d18ed0479fb6d594a13fa0b7a09e
aletutaj <aletutaj@gmail.com>
2023-09-27 22:08:03 +0200

    linking subpages

--- renovation budget management/src/App.jsx
+++ renovation budget management/src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css'
 import Header from "./jsx/Header.jsx";
 import Footer from "./jsx/Footer.jsx";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import React from "react";
+import TablePage from "./jsx/TablePage.jsx";
 
 
 function App() {
 
-  return (
-    <>
-        <div>
-            <Header />
-            <Footer />
-        </div>
-    </>
-  )
+    return (
+        <Router>
+            <div>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={TablePage}/>
+                    <Route path="/salon" component={TablePage}/>
+                    <Route path="/kuchnia" component={TablePage}/>
+                    <Route path="/lazienka" component={TablePage}/>
+                    <Route path="/gabinet" component={TablePage}/>
+                    <Route path="/sypialnia" component={TablePage}/>
+                </Routes>
+                <Footer/>
+            </div>
+        </Router>
+    )
 }
 
 export default App
--- renovation budget management/src/jsx/Header.jsx
+++ renovation budget management/src/jsx/Header.jsx
@@ -1,47 +1,105 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../scss/elements/_header.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronDown, faHome, faUtensils, faBath, faBriefcase, faBed} from '@fortawesome/free-solid-svg-icons';
+import {
+    faHome,
+    faUtensils,
+    faBath,
+    faBriefcase,
+    faBed,
+    faChevronDown,
+    faGlobe
+} from '@fortawesome/free-solid-svg-icons';
+import {Link, useLocation} from 'react-router-dom';
 
 const Header = () => {
     const [menuVisible, setMenuVisible] = useState(false);
+    const location = useLocation();
+    const [headerText, setHeaderText] = useState('Zarządzanie budżetem remontowym');
 
     const toggleMenu = () => {
         setMenuVisible(!menuVisible);
     };
 
+    const handleMenuItemClick = () => {
+        setMenuVisible(false);
+    };
+
+    const handleHomeIconClick = (e) => {
+        if (menuVisible) {
+            e.preventDefault();
+        }
+    };
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/salon':
+                setHeaderText('Salon');
+                break;
+            case '/kuchnia':
+                setHeaderText('Kuchnia');
+                break;
+            case '/lazienka':
+                setHeaderText('Łazienka');
+                break;
+            case '/gabinet':
+                setHeaderText('Gabinet');
+                break;
+            case '/sypialnia':
+                setHeaderText('Sypialnia');
+                break;
+            default:
+                setHeaderText('Zarządzanie budżetem remontowym');
+                break;
+        }
+    }, [location.pathname]);
+
+
     return (
         <header className="header">
             <div className="menu" onClick={toggleMenu}>
-                <span>Zarządzanie budżetem remontowym</span>
+                <Link to="/" onClick={handleHomeIconClick}>
+                    <FontAwesomeIcon icon={faGlobe}/>
+                </Link>
+                <span>{headerText}</span>
                 <FontAwesomeIcon icon={faChevronDown}/>
             </div>
             {menuVisible && (
                 <ul className="submenu">
                     <li>
-                        <FontAwesomeIcon icon={faHome}/>
-                        <span>Salon</span>
+                        <Link to="/salon" onClick={handleMenuItemClick}>
+                            <FontAwesomeIcon icon={faHome}/>
+                            <span> Salon</span>
+                        </Link>
                     </li>
                     <li>
-                        <FontAwesomeIcon icon={faUtensils}/>
-                        <span>Kuchnia</span>
+                        <Link to="/kuchnia" onClick={handleMenuItemClick}>
+                            <FontAwesomeIcon icon={faUtensils}/>
+                            <span> Kuchnia</span>
+                        </Link>
                     </li>
                     <li>
-                        <FontAwesomeIcon icon={faBath}/>
-                        <span>Łazienka</span>
+                        <Link to="/lazienka" onClick={handleMenuItemClick}>
+                            <FontAwesomeIcon icon={faBath}/>
+                            <span> Łazienka</span>
+                        </Link>
                     </li>
                     <li>
-                        <FontAwesomeIcon icon={faBriefcase}/>
-                        <span>Gabinet</span>
+                        <Link to="/gabinet" onClick={handleMenuItemClick}>
+                            <FontAwesomeIcon icon={faBriefcase}/>
+                            <span> Gabinet</span>
+                        </Link>
                     </li>
                     <li>
-                        <FontAwesomeIcon icon={faBed}/>
-                        <span>Sypialnia</span>
+                        <Link to="/sypialnia" onClick={handleMenuItemClick}>
+                            <FontAwesomeIcon icon={faBed}/>
+                            <span> Sypialnia</span>
+                        </Link>
                     </li>
                 </ul>
             )}
         </header>
     );
-}
+};
 
 export default Header;
