98b2c75510ff0a3569f394b2ad5dafe4008ebddd
aletutaj <aletutaj@gmail.com>
2023-10-06 19:22:54 +0200

    element home - modification

--- renovation budget management/src/jsx/Home.jsx
+++ renovation budget management/src/jsx/Home.jsx
@@ -2,31 +2,16 @@ import  { useEffect, useState } from 'react';
 import SubpageTable from './SubpageTable';
 import { db } from '../firebase';
 import {collection, getDocs} from 'firebase/firestore';
-// import {addDoc, collection, getDocs} from 'firebase/firestore';
 
 const Home = () => {
     const [allItems, setAllItems] = useState([]);
-    // const [newItem, setNewItem] = useState({ name: '', quantity: 0, price: 0 });
 
-    // const handleAddItem = async () => {
-    //     try {
-    //         const newItemRef = collection(db, '/');
-    //         const docRef = await addDoc(newItemRef, newItem);
-    //         const newItemWithId = { id: docRef.id, ...newItem };
-    //         setAllItems([...allItems, newItemWithId]);
-    //         console.log('Data added to the database.');
-    //     } catch (error) {
-    //         console.error('Error while adding data:', error);
-    //     }
-    //
-    //     setNewItem({ name: '', quantity: 0, price: 0 });
-    // };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const allData = [];
-                const roomNames = ['salon', 'lazienka','gabinet', 'sypialnia', 'kuchnia' ];
+                const roomNames = ['salon', 'lazienka','gabinet', 'sypialnia', 'kuchnia'];
                 for (const roomName of roomNames) {
                     const roomData = await getDataFromRoom(roomName);
                     allData.push(...roomData);
@@ -53,8 +38,6 @@ const Home = () => {
     return (
         <div>
             <SubpageTable items={allItems} setItems={setAllItems} />
-            <div className="add-item-container">
-            </div>
         </div>
     );
 };
