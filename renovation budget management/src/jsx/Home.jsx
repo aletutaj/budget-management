8792970b3c6ad39d91d3d66ec4946835b945d1c8
aletutaj <aletutaj@gmail.com>
2023-10-06 19:28:38 +0200

    edited Home component

--- renovation budget management/src/jsx/Home.jsx
+++ renovation budget management/src/jsx/Home.jsx
@@ -1,17 +1,16 @@
-import  { useEffect, useState } from 'react';
-import SubpageTable from './SubpageTable';
+import { useEffect, useState } from 'react';
 import { db } from '../firebase';
-import {collection, getDocs} from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Home = () => {
     const [allItems, setAllItems] = useState([]);
 
-
     useEffect(() => {
         const fetchData = async () => {
             try {
+                const roomNames = ['salon', 'lazienka', 'gabinet', 'sypialnia', 'kuchnia'];
                 const allData = [];
-                const roomNames = ['salon', 'lazienka','gabinet', 'sypialnia', 'kuchnia'];
+
                 for (const roomName of roomNames) {
                     const roomData = await getDataFromRoom(roomName);
                     allData.push(...roomData);
@@ -31,13 +30,43 @@ const Home = () => {
         const data = querySnapshot.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
+            'cena łącznie': doc.data().quantity * doc.data().price,
         }));
         return data;
     };
 
+    const getTotalPrice = () => {
+        return allItems.reduce((total, item) => total + item['cena łącznie'], 0);
+    };
+
     return (
         <div>
-            <SubpageTable items={allItems} setItems={setAllItems} />
+            <table className="subpage-table">
+                <thead>
+                <tr>
+                    <th>Nazwa</th>
+                    <th>Ilość</th>
+                    <th>Cena</th>
+                    <th>Cena łącznie</th>
+                </tr>
+                </thead>
+                <tbody>
+                {allItems.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.name}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.price}</td>
+                        <td>{item['cena łącznie'].toFixed(2)}</td>
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td colSpan="3">Podsumowanie wydatków</td>
+                    <td>{getTotalPrice().toFixed(2)}</td>
+                </tr>
+                </tfoot>
+            </table>
         </div>
     );
 };
